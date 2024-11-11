721456537d49187c0e95308ab451c90c5235d586
sidhart25 <navneetnishchal1420@gmail.com>
2024-11-12 03:57:46 +0530

    learnt so much in previous month

---
+++ bookish/nodejsDesignPatterns/design-patterns-1.js
@@ -0,0 +1,10 @@
+// The design patterns explored in this chapter are as follows:
+// • Factory
+// • Proxy
+// • Decorator
+// • Adapter
+// • Strategy
+// • State
+// • Template
+// • Middleware
+// • Command
---
+++ factoryPatterns.js
@@ -0,0 +1,2 @@
+//  In JavaScript, any function can return an object.When it does so without the new
+// keyword, it’s a factory function.
